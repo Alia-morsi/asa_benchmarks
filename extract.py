6b8d2f2747c9ae65636efce5cb57dd679cd002ad
Alia <alia.morsi@aucegypt.edu>
2022-02-04 17:08:14 +0100

    added ability to sonify the interpolated ground truth alignments to verify the extent of their reliability

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ import pandas as pd
 import lib.midi as midilib
 import lib.util as util
 import shutil
+import soundfile as sf
 
 from scipy.io import wavfile
 
@@ -62,6 +63,9 @@ def restructure_asap_files(asap_root, metadata_path):
     #for now, just filter the bachs
     bach_subset_df = asap_metadata_df[asap_metadata_df['composer'] == 'Bach']
     
+    #delete any rows that have audio_performance set to nan
+    bach_subset_df = bach_subset_df[bach_subset_df['audio_performance'].notnull()]
+    
     #this function should overwrite if exists also.
     for index, row in bach_subset_df.iterrows():
         basename = row['title']
@@ -71,8 +75,31 @@ def restructure_asap_files(asap_root, metadata_path):
                     
         shutil.copy(os.path.join(asap_root, row['midi_performance']), 'data/perf/{}'.format(basename + '.midi'))
         shutil.copy(os.path.join(asap_root, row['performance_annotations']), 'data/perf/{}'.format(basename + '.txt'))
+        shutil.copy(os.path.join(asap_root, row['audio_performance']), 'data/perf/{}'.format(basename + '.wav')) 
         
 
+#we'll just pass in the default paths
+def sonify_interpolated_gt():
+    os.makedirs('eval/sonic', exist_ok=True)
+    
+    interpolation_basepath = 'align/ground-beat-interpol'
+    gt_alignments = sorted([f for f in os.listdir(interpolation_basepath) if f.endswith('.txt')])
+    
+    #parallel to the interpolated ground truths  
+    perf_audios = ['{}.wav'.format(os.path.join('data/perf', gt_alignment[:-len('.txt')])) for gt_alignment in gt_alignments]
+    perf_midis = ['{}.midi'.format(os.path.join('data/perf', gt_alignment[:-len('.txt')])) for gt_alignment in gt_alignments]  
+    score_midis = ['{}.midi'.format(os.path.join('data/score', gt_alignment[:-len('.txt')])) for gt_alignment in gt_alignments]
+    
+    for gt_alignment, perf_audio, perf_midi_file, score_midi_file in zip(gt_alignments, perf_audios, perf_midis, score_midis):
+        if not os.path.exists(perf_audio) or not os.path.exists(perf_midi_file) or not os.path.exists(score_midi_file):
+            continue
+            
+        stereo_sonification = util.sonic_gt_evaluation(os.path.join(interpolation_basepath, gt_alignment), perf_audio, perf_midi_file, score_midi_file)
+        
+        outfile = os.path.join('eval/sonic/', '{}'.format(gt_alignment[:-len('.txt')] + '.wav'))
+        print('Writing {} \n'.format(outfile))
+        sf.write(outfile, stereo_sonification.T, 44100)
+    return
 
 if __name__ == "__main__":
     os.makedirs('data/perf',exist_ok=True)
