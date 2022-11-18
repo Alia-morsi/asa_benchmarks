a33681ab7495a58439a5c7ac8b9130219ae0141e
Alia <alia.morsi@aucegypt.edu>
2022-11-18 19:56:53 +0100

    removing unused files

--- lib/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    name="gtalign", 
-    ext_modules=cythonize("gtalign.pyx"),
-    include_dirs=[numpy.get_include()]
-) 
